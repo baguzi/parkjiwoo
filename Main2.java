a9a0b45c81d8799ee2277bb83228fd624d49398e
parkjiwoo <bagjiw@naver.com>
2025-03-24 14:42:33 +0900

    실습예제2

---
+++ Main2.java
@@ -0,0 +1,8 @@
+package java;
+public class Main {
+    public static void main(String[] args) {
+        String name = "박지우";
+        int age = 18;
+        System.out.println(name + "님은" +  age + "살 입니다");
+    }
+}

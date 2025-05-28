2888087bfcbe19d4a2d3010a621a44e13879b963
parkjiwoo <bagjiw@naver.com>
2025-05-28 23:05:52 +0900

    기말고사

---
+++ final/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class changePeople {
+    int maxPerson;
+
+
+    public changePeople (int people) {
+        maxPerson= people;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("엘레베이터 탑승 인원");
+
+
+
+
+
+
+    }
+}

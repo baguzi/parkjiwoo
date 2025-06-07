d5ec00011e9992c875893fa48397bd0654737f1e
parkjiwoo <bagjiw@naver.com>
2025-06-07 15:29:59 +0900

    기말고사

--- final/src/Main.java
+++ final/src/Main.java
@@ -1,24 +1,32 @@
 import java.util.Scanner;
 
-class changePeople {
-    int maxPerson;
+class Elevator {
+    int maxPeople;
+    int changePeople;
 
+    public Elevator(int max) {
+        maxPeople = max;
+    }
 
-    public changePeople (int people) {
-        maxPerson= people;
+    public void setPeople(int people) {
+        changePeople = people;
+    }
+
+    public void checkPeople() {
+        System.out.println("현재 인원: " + changePeople + "명");
+
+        if (changePeople >= maxPeople) {
+            System.out.println("계단을 이용해 주세요");
+        } else if (changePeople >= maxPeople - 2) {
+            System.out.println("약간 혼잡합니다. 급하면 계단을 이용해주세요");
+        } else {
+            System.out.println("엘리베이터를 이용하셔도 됩니다.");
+        }
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-
-        System.out.println("엘레베이터 탑승 인원");
-
-
-
-
 
 
     }

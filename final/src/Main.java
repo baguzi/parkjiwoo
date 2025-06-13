cf65f35eea9b43466da70d3851345263dbe081bf
parkjiwoo <bagjiw@naver.com>
2025-06-13 21:31:07 +0900

    기말고사

--- final/src/Main.java
+++ final/src/Main.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
 
 class Elevator {
-    int maxPeople;
-    int changePeople;
+    private int maxPeople;
+    private int changePeople;
 
-    public Elevator(int max)
-    {
-        maxPeople = max;
+    public Elevator(int max) {
+        this.maxPeople = max;
     }
 
     public void setPeople(int people) {
-        changePeople = people;
+        this.changePeople = people;
     }
 
-    public void checkPeople() {
-        System.out.println("현재 인원: " + changePeople + "명");
+    public int getPeople() {
+        return changePeople;
+    }
 
+    public String checkPeopleStatus() {
         if (changePeople >= maxPeople) {
-            System.out.println("계단을 이용해 주세요");
+            return "계단을 이용해 주세요";
         } else if (changePeople >= maxPeople - 2) {
-            System.out.println("약간 혼잡합니다. 급하면 계단을 이용해주세요");
+            return "약간 혼잡합니다. 급하면 계단을 이용해주세요";
         } else {
-            System.out.println("엘리베이터를 이용하셔도 됩니다.");
+            return "엘리베이터를 이용하셔도 됩니다.";
         }
     }
 }
@@ -30,16 +31,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
         System.out.print("엘리베이터 최대 인원을 입력하세요: ");
         int max = scanner.nextInt();
 
         Elevator elevator = new Elevator(max);
 
-        System.out.print("현재 탑승 인원을 입력하세요: ");
-        int people = scanner.nextInt();
+        while (true) {
+            System.out.print("현재 탑승 인원을 입력하세요 (0 이하 입력 시 종료): ");
+            int people = scanner.nextInt();
 
-        elevator.setPeople(people);
-        elevator.checkPeople();
+            if (people <= 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+
+            elevator.setPeople(people);
+            System.out.println("현재 인원: " + elevator.getPeople() + "명");
+            System.out.println(elevator.checkPeopleStatus());
+        }
     }
 }
+
